faa72a997bb6be5a946dafa46365add93d4c0df1
Scratchaker <105060449+Scratchaker@users.noreply.github.com>
2025-09-28 18:52:51 +0200

    Add files via upload

---
+++ PythonLauncher.bat
@@ -0,0 +1,133 @@
+@echo off
+setlocal enabledelayedexpansion
+
+
+
+:::------------------------------------Configuration------------------------------------:::
+
+::Set if the py cli laucher is being used. When Enabled pythondir MUST be "py" and the version to use can be configured with pythonversion
+set usepylancher=1
+set pythonversion=3.11
+::Used to configure the python exe to use. Useful when using portable python versions
+set pythondir=py
+
+::Used to define the title of the cmd that will execute the python file
+set windowname=Python
+
+::This script can auto detect what initial files to run edit this list to add custom initial files or change the order(if two files exist the first will be runed)
+set initialfiles="run.py" "main.py" "app.py"
+
+::Set if a venv will be created before runing the python file
+set usevenv=1
+::Used to define the name of the venv that will be created and used
+set venvname=pyvenv
+::Set if a requirements.txt file will be installed when creating the venv. Packages will not be installed or updated if the venv alredy exists
+set installrequirementsfile=1
+::Used to define the requirements file that will be installed
+set requirementsfile=requirements.txt
+
+::Set if the cmd thet runs the python file should start minimized
+set minimizedcmd=0
+::Used to toggle if the arguments passed to this file will be passed to the python file. Will disable running a python file when dragging it over this file if enabled
+set passarguments=0
+
+
+
+:::------------------------------------Code------------------------------------:::
+rem Check if python is installed.
+%pythondir% -V
+if %ERRORLEVEL% neq 0 (
+    rem Python is NOT installed.
+	cls
+	echo No python version was found!
+	echo Opening download page.
+	start https://www.python.org/downloads/
+	pause >nul
+	exit
+)
+cls
+
+rem Define what to do.
+if "!minimizedcmd!"=="1" (
+    set "min=/min"
+	set "minexit=& exit"
+) else (
+    set "min="
+	set "minexit="
+)
+
+if "!usevenv!"=="1" (
+    set "venvcmd=%cd%\%venvname%\Scripts\activate"
+) else (
+    set "venvcmd=cls"
+)
+
+if "!usepylancher!"=="1" (
+    set "pycmd=-%pythonversion%"
+) else (
+    set "pycmd="
+)
+
+
+if !passarguments!==1 (
+	rem Run a flie with arguments.
+	set foundFile=
+	for %%f in (%initialfiles%) do (
+		if exist %%~f (
+			set foundFile=%%~f
+			set "command=start %min% "%windowname%" cmd /k ^"call !venvcmd! ^& %pythondir% %pycmd% !foundFile! %* %minexit%^""
+			goto :runFile
+		)
+    )
+	set command=start %min% "%windowname%" cmd /k "!venvcmd! & %pythondir% %pycmd%"
+	goto :runFile
+) else (
+    if "%~1"=="" (
+		rem No argument provided.
+		rem Check if any initial file exists.
+		set foundFile=
+		for %%f in (%initialfiles%) do (
+			if exist %%~f (
+				set foundFile=%%~f
+				set command=start %min% "%windowname%" cmd /k "!venvcmd! & %pythondir% %pycmd% !foundFile! %minexit%"
+				goto :runFile
+			)
+        )
+		set command=start %min% "%windowname%" cmd /k "!venvcmd! & %pythondir% %pycmd%"
+		goto :runFile
+	) else (
+		rem An argument has been provided. Run that file.
+		set command=start %min% "%windowname%" cmd /k "!venvcmd! & %pythondir% %pycmd% %1 %minexit%"
+		goto :runFile
+	)
+)
+
+:runFile
+rem Run command.
+call :createvenv
+%command%
+goto :eof
+
+:createvenv
+rem Create a virtual environment.
+if "!usevenv!"=="1" (
+    if not exist "%venvname%" (
+		echo Creating Virtual environment...
+		%pythondir% %pycmd% -m venv "%venvname%"
+		
+		if "!installrequirementsfile!"=="1" (
+			if exist "%requirementsfile%" (
+				%cd%\%venvname%\Scripts\activate
+				%pythondir% %pycmd% -m pip install -r %requirementsfile%
+			) else (
+				echo No requirements to install
+			)
+		) else (
+			echo No requirements to install
+		)
+	)
+
+) else (
+    echo VENV is disabled
+)
+goto :eof
